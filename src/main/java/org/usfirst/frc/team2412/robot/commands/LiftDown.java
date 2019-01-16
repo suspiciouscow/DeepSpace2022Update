6e1249300401a503ea0067ebe5009fd9fcc1d359
s-notarangeloj <s-notarangeloj@52S500036380.it.bsd405.org>
2019-01-15 17:19:29 -0800

    adding liftdown

--- src/main/java/org/usfirst/frc/team2412/robot/commands/LiftDown.java
+++ src/main/java/org/usfirst/frc/team2412/robot/commands/LiftDown.java
@@ -3,45 +3,11 @@ package org.usfirst.frc.team2412.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class LiftDown extends Command {
+public class LiftDown extends CommandBase {
 
 	public LiftDown() {
 		// TODO Auto-generated constructor stub
-	}
-
-	public LiftDown(String name) {
-		super(name);
-		// TODO Auto-generated constructor stub
-	}
-
-	public LiftDown(double timeout) {
-		super(timeout);
-		// TODO Auto-generated constructor stub
-	}
-
-	public LiftDown(Subsystem subsystem) {
-		super(subsystem);
-		// TODO Auto-generated constructor stub
-	}
-
-	public LiftDown(String name, Subsystem subsystem) {
-		super(name, subsystem);
-		// TODO Auto-generated constructor stub
-	}
-
-	public LiftDown(double timeout, Subsystem subsystem) {
-		super(timeout, subsystem);
-		// TODO Auto-generated constructor stub
-	}
-
-	public LiftDown(String name, double timeout) {
-		super(name, timeout);
-		// TODO Auto-generated constructor stub
-	}
-
-	public LiftDown(String name, double timeout, Subsystem subsystem) {
-		super(name, timeout, subsystem);
-		// TODO Auto-generated constructor stub
+		requires(liftSubsystem);
 	}
 
 	@Override
