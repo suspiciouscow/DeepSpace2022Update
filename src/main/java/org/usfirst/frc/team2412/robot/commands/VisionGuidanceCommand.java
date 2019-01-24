22aee2d7bd4909c86951c4919e3dda65ea1b4e5b
s-engt <s-engt@52S50012454.it.bsd405.org>
2019-01-23 19:54:02 -0800

    Revert "oops"
    This reverts commit 2ddb5a26508e90ccb47e00d93af6ce29911fa01d.

--- src/main/java/org/usfirst/frc/team2412/robot/commands/VisionGuidanceCommand.java
+++ src/main/java/org/usfirst/frc/team2412/robot/commands/VisionGuidanceCommand.java
@@ -3,62 +3,54 @@ package org.usfirst.frc.team2412.robot.commands;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
-import java.text.ParseException;
 
 public class VisionGuidanceCommand extends CommandBase {
-	private static final int PORT = 1111;
+    private static final int PORT = 1111;
 
-	private DatagramSocket socket = null;
-	private DatagramPacket packet;
-	private byte[] receiveData = new byte[1024];
+    private DatagramSocket socket = null;
+    private DatagramPacket packet;
+    private byte[] receiveData = new byte[1024];
 
-	// private boolean teleopHappened = false;
+    // private boolean teleopHappened = false;
 
-	public VisionGuidanceCommand() {
-		requires(tempDriveBase);
-	}
+    public VisionGuidanceCommand() {
+        requires(tempDriveBase);
+    }
 
-	@Override
-	protected void initialize() {
-		try {
-			socket = new DatagramSocket(PORT);
-		} catch(SocketException e) {
-			System.err.println("Could not create socket: ");
-			e.printStackTrace();
-		}
-	}
+    @Override
+    protected void initialize() {
+        try {
+            socket = new DatagramSocket(PORT);
+        } catch(SocketException e) {
+            System.err.println("Could not create socket: ");
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	protected void execute() {
-		// tempDriveBase.drive(Robot.m_oi.stick);
-		try {
-		packet = new DatagramPacket(receiveData, receiveData.length);
-		System.out.println("Receiving message...");
-		socket.receive(packet);
-		
-		receiveData = packet.getData();
-		String receiveString = (new String(receiveData)).trim();
-		// System.out.println(receiveString.length());
-		System.out.println(receiveString);
-		
-		// Parse the string into angle, distance, doextake, and targets found values.
-		String[] visionData = receiveString.split(";");
-		if(visionData.length != 4) {
-			throw new ParseException("Not the right number of values received!", visionData.length - 1);
-		}
-		d
-		receiveData = new byte[1024]; // Clear the data.
-		} catch(Exception e) {
-			System.out.println("Exception happened");
-		e.printStackTrace();
-		}
-	}
+    @Override
+    protected void execute() {
+        // tempDriveBase.drive(Robot.m_oi.stick);
+        try {
+            packet = new DatagramPacket(receiveData, receiveData.length);
+            System.out.println("Receiving message...");
+            socket.receive(packet);
+    
+            receiveData = packet.getData();
+            String receiveString = (new String(receiveData)).trim();
+            // System.out.println(receiveString.length());
+            System.out.println(receiveString);
+    
+            receiveData = new byte[1024]; // Clear the data.
+          } catch(Exception e) {
+            System.out.println("exception happened");
+          }
+    }
 
-	@Override
-	protected void end() {
-		if(socket != null) {
-			socket.close();
-			System.out.println("Closing...");
-		}
-	}
+    @Override
+    protected void end() {
+        if(socket != null) {
+            socket.close();
+            System.out.println("Closing...");
+        }
+    }
 }
