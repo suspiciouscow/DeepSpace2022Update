f32250955b67400567ffae53f28145b2f4a5b063
Robototes Programmers <programmers@robototes.com>
2019-01-26 10:27:10 -0800

    Added USB camera.

--- src/main/java/org/usfirst/frc/team2412/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team2412/robot/Robot.java
@@ -7,9 +7,15 @@
 
 package org.usfirst.frc.team2412.robot;
 
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team2412.robot.commands.CommandBase;
 import org.usfirst.frc.team2412.robot.subsystems.ExampleSubsystem;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -42,6 +48,29 @@ public class Robot extends TimedRobot {
 //		m_chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", m_chooser);
+
+		Thread visionThread = new Thread(() -> {
+			System.out.println("In thread");
+			UsbCamera camera = new UsbCamera("Microsoft Lifecam", "/dev/video0");
+			CameraServer.getInstance().addCamera(camera);
+			camera.setResolution(640, 480);
+
+			CvSink cvSink = CameraServer.getInstance().getVideo();
+			CvSource outputStream = CameraServer.getInstance().putVideo("UsbCamera", 640, 480);
+
+			Mat mat = new Mat();
+
+			while (!Thread.interrupted()) {
+				if (cvSink.grabFrame(mat) == 0) {
+					outputStream.notifyError(cvSink.getError());
+					continue;
+				}
+				Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
+				outputStream.putFrame(mat);
+			}
+		});
+		visionThread.setDaemon(true);
+		visionThread.start();
 	}
 
 	/**
