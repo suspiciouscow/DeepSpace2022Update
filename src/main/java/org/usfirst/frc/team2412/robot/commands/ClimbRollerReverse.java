8e9915fde0a4ebc129b23ae37dd2da5244f25a5d
s-glocknera <s-glocknera@52S500037469.it.bsd405.org>
2019-01-25 17:16:01 -0800

    I think I finished 2/3 of the Climb Programming. It's sad that I've done more work then the whole Climb Sub-team considering I'm the person and that my main team for programming is In-Take/Yeeeeeeeeeeeeeeeeeeeeeeet.

--- src/main/java/org/usfirst/frc/team2412/robot/commands/ClimbRollerReverse.java
+++ src/main/java/org/usfirst/frc/team2412/robot/commands/ClimbRollerReverse.java
@@ -2,4 +2,16 @@ package org.usfirst.frc.team2412.robot.commands;
 
 public class ClimbRollerReverse extends CommandBase {
 
+	public ClimbRollerReverse() {
+		requires(ClimbRoller);
+	}
+
+	public void execute() {
+		ClimbRoller.ClimbRollerReverese();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return true;
+	}
 }
