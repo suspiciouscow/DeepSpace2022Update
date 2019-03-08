5954135045c4e199ad0233d0e6c07fd0fdf47e4d
Timothy Eng <timothyeng36@gmail.com>
2019-03-08 13:58:37 -0800

    Run both ClimbRollerAxis and ClimbRollerJoystick in parallel.

--- src/main/java/org/usfirst/frc/team2412/robot/commands/DeployRailsSequence.java
+++ src/main/java/org/usfirst/frc/team2412/robot/commands/DeployRailsSequence.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class DeployRailsSequence extends CommandGroup {
     public DeployRailsSequence() {
         addSequential(new DeployRails());
-        addSequential(new ClimbRollerAxis());
-        addSequential(new ClimbLiftJoystick());
+        addParallel(new ClimbRollerAxis());
+        addParallel(new ClimbLiftJoystick());
     }
 }
