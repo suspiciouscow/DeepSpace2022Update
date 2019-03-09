76579bba8196b13b37cd7741d38a7d4ad5aec05d
Timothy Eng <timothyeng36@gmail.com>
2019-03-09 14:12:42 -0800

    Temporariy disabled physical robot functions for testing.

--- src/main/java/org/usfirst/frc/team2412/robot/subsystems/ClimbRollerSubsystem.java
+++ src/main/java/org/usfirst/frc/team2412/robot/subsystems/ClimbRollerSubsystem.java
@@ -1,14 +1,11 @@
 package org.usfirst.frc.team2412.robot.subsystems;
 
-import org.usfirst.frc.team2412.robot.RobotMap;
-
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class ClimbRollerSubsystem extends Subsystem {
 
-	private VictorSP victorSP3 = RobotMap.victorSP3;
+	// private VictorSP victorSP3 = RobotMap.victorSP3;
 
 	@Override
 	protected void initDefaultCommand() {
@@ -16,24 +13,24 @@ public class ClimbRollerSubsystem extends Subsystem {
 	}
 
 	public void ClimbRollerForward() {
-		double rollerSpeed = 0.75;
+		// double rollerSpeed = 0.75;
 		// climbRoller1.set(0.75);
-		victorSP3.set(rollerSpeed);
+		// victorSP3.set(rollerSpeed);
 	}
 
 	public void ClimbRollerReverese() {
-		double rollerSpeed = -0.75;
+		// double rollerSpeed = -0.75;
 		// climbRoller1.set(rollerSpeed);
-		victorSP3.set(rollerSpeed);
+		// victorSP3.set(rollerSpeed);
 	}
 
 	public void ClimbRollerStop() {
-		victorSP3.set(0.0);
+		// victorSP3.set(0.0);
 		System.out.println("released");
 	}
 	
 	public void ClimbRollerAxis(Joystick stick, int axis) {
-		victorSP3.set(stick.getRawAxis(axis));
+		// victorSP3.set(stick.getRawAxis(axis));
 	}
 
 }
--- src/main/java/org/usfirst/frc/team2412/robot/subsystems/InTakeCargoSubsystem.java
+++ src/main/java/org/usfirst/frc/team2412/robot/subsystems/InTakeCargoSubsystem.java
@@ -1,16 +1,12 @@
 package org.usfirst.frc.team2412.robot.subsystems;
 
-import org.usfirst.frc.team2412.robot.RobotMap;
-
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class InTakeCargoSubsystem extends Subsystem {
 
 	// RESEARCH TALONSRX CAPABILITIES AND EDUCATE THE TEAM
-	private WPI_VictorSPX CargoMotor1 = RobotMap.cargoMotor1;
+	// private WPI_VictorSPX CargoMotor1 = RobotMap.cargoMotor1;
 
 	@Override
 	protected void initDefaultCommand() {
@@ -18,18 +14,18 @@ public class InTakeCargoSubsystem extends Subsystem {
 
 	// CHANGE MOTOR SPEED TO CONSTANTS
 	public void InTakeCargo() {
-		CargoMotor1.set(1);
+		// CargoMotor1.set(1);
 	}
 
 	public void OutputCargo() {
-		CargoMotor1.set(-1);
+		// CargoMotor1.set(-1);
 	}
 
 	public void InTakeStop() {
-		CargoMotor1.set(0.0);
+		// CargoMotor1.set(0.0);
 	}
 	
 	public void InTakeCargoAxis(Joystick stick, int axis) {
-		CargoMotor1.set(stick.getRawAxis(axis));
+		// CargoMotor1.set(stick.getRawAxis(axis));
 	}
 }
