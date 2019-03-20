d2f8da6b0f7094638ca5f859467047826f6ca94d
Timothy Eng <timothyeng36@gmail.com>
2019-03-20 12:41:57 -0700

    Don't deploy the rails but lift to preset cargo 2 and rotate intake.

--- src/main/java/org/usfirst/frc/team2412/robot/commands/DeployRailsSequence.java
+++ src/main/java/org/usfirst/frc/team2412/robot/commands/DeployRailsSequence.java
@@ -4,7 +4,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class DeployRailsSequence extends CommandGroup {
     public DeployRailsSequence() {
-        addSequential(new DeployRails());
+        // addSequential(new DeployRails());
+        addSequential(new GoToLevel(2, false));
+        addSequential(new GoToIntakeAngle(170));
         addParallel(new ClimbRollerAxis());
         addParallel(new ClimbLiftJoystick());
     }
