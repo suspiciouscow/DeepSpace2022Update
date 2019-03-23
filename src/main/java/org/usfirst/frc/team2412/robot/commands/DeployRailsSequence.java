bd176e0d79a5f488e02992d0c020d54597af2f68
Timothy Eng <timothyeng36@gmail.com>
2019-03-23 08:23:41 -0700

    Changed back to original climb with rail deployment.

--- src/main/java/org/usfirst/frc/team2412/robot/commands/DeployRailsSequence.java
+++ src/main/java/org/usfirst/frc/team2412/robot/commands/DeployRailsSequence.java
@@ -1,15 +1,10 @@
 package org.usfirst.frc.team2412.robot.commands;
 
-import org.usfirst.frc.team2412.robot.OI;
-
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class DeployRailsSequence extends CommandGroup {
     public DeployRailsSequence() {
-        // addSequential(new DeployRails());
-        // addSequential(new GoToLevel(2, false));
-        addSequential(new GoToHeight(57));
-        addSequential(new GoToIntakeAngle(OI.intakeDownAngle));
+        addSequential(new DeployRails());
         addParallel(new ClimbRollerAxis());
         addParallel(new ClimbLiftJoystick());
     }
