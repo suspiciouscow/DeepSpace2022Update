2b6d7fad78b0a0e7f482bc732cc5a5b742f6c0f5
Timothy Eng <timothyeng36@gmail.com>
2019-07-26 11:26:48 -0500

    Lower turning and driving speed when we're not in safe mode.

--- src/main/java/org/usfirst/frc/team2412/robot/subsystems/DriveBaseSubsystem.java
+++ src/main/java/org/usfirst/frc/team2412/robot/subsystems/DriveBaseSubsystem.java
@@ -22,6 +22,6 @@ public class DriveBaseSubsystem extends Subsystem {
 
 	public void drive(Joystick stick) {
 		// robotDrive.arcadeDrive(stick.getY(), Math.pow(-stick.getTwist() * 0.8, 3), true);
-		robotDrive.arcadeDrive(((RobotMap.SAFE_MODE)?Math.min(Math.max(stick.getY(),-0.4), 0.4):stick.getY()),-1* ((RobotMap.SAFE_MODE)?Math.min(Math.max(stick.getTwist(),-0.4), 0.4):stick.getTwist()), true);
+		robotDrive.arcadeDrive(((RobotMap.SAFE_MODE)?Math.min(Math.max(stick.getY(),-0.4), 0.4):0.7*stick.getY()),-1* ((RobotMap.SAFE_MODE)?Math.min(Math.max(stick.getTwist(),-0.4), 0.4):stick.getTwist()*0.5), true);
 	}
 }
